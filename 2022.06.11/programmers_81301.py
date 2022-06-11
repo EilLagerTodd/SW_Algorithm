bf3121871255874bec70a3d199460c53c3187d0a
hariose <67162120+hariose@users.noreply.github.com>
2022-06-11 22:52:27 +0900

    코드 수정

--- 2022.06.11/programmers_81301.py
+++ 2022.06.11/programmers_81301.py
@@ -6,15 +6,12 @@ def listToString(str_list):
 
 
 def solution(s):
-
     if s.isnumeric():
         return int(s)
-
     word = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,
             "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
     list = []
     number_str = []
-
     for i in s:
         if i.isnumeric():
             list.append(i)
@@ -27,11 +24,5 @@ def solution(s):
             else:
                 list.append(str(word[result]))
                 number_str = []
-
     number = listToString(list)
-
     return int(number)
-
-
-s = "one4seveneight"
-print(solution(s))
